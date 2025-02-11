d1f610c6262343dda54314223af95db7cdc9f5a0
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-02-11 16:02:21 +0000

    Latest versions of the Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,18 +6,19 @@ import userEvent from '@testing-library/user-event';
 
 // const images = ["https://picsum.photos/id/16/2500/1667.jpg", "https://picsum.photos/id/20/3670/2462.jpg","https://picsum.photos/id/10/2500/1667.jpg"]
   
-// test('ImageSelector renders activeThumbnail css class ', () => {
-//   const user = userEvent.setup()
-//   const component =  render(<ImageSelector src ={images[0]}/>);
-//   await user.click(screen.getByRole('img', {name: /click me!/i}))
-//   const textElement = getByText(/Hello world/i);
-//   expect(textElement);
-// });
+test('ImageSelector changes currentImage state on click', async () => {
+  const user = userEvent.setup();
+  render(<App />);
+  const images = screen.getAllByRole('img');
+  // const component =  render(<ImageSelector onClick={} src ={imagesUrls[0]} className=''/>);
+  await user.click(images[0])
+  const textElement = getByText(/Hello world/i);
+  expect(textElement);
+});
 
 test('src contains correct value', () => {
-  render(<ImageSelector onClick={undefined} src={imageUrls[0]} className=''/>)
+  render(<ImageSelector onClick={jest.fn()} src={imageUrls[0]} className=''/>)
   const testImage = document.querySelector("img") as HTMLImageElement;
   expect(testImage.src).toContain("https://picsum.photos/id/600/1600/900.jpg");
   expect(testImage.src).not.toContain("https://picsum.photos/id/601/1600/900.jpg");
-
 })
